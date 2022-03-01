8384fb80f2631dabc09af782e4f82778ac8940d6
Han-Lon <56945456+Han-Lon@users.noreply.github.com>
2022-02-28 21:49:21 -0600

    Add encryption tests to CICD

--- __test__/rawrify-github-http-checks.py
+++ __test__/rawrify-github-http-checks.py
@@ -11,6 +11,9 @@ else:
     rawrify_domain = "dev.rawrify.com"
 
 # Dict of routes to check
+# SYNTAX
+# GET requests: "name@GET": "url"
+# POST requests: "name@POST": ["url", "payload"]
 routes = {
     "ipv4@GET": f"https://ipv4.{rawrify_domain}/ip",
     # "ipv6": "https://ipv6.rawrify.com/ip", Can't test IPv6 because GitHub self-hosted runners don't support IPv6 at all https://github.com/actions/virtual-environments/issues/668
@@ -22,7 +25,9 @@ routes = {
     "location-country-name@GET": f"https://www.{rawrify_domain}/location?country-name",
     "location-city@GET": f"https://www.{rawrify_domain}/location?city",
     "location-coords@GET": f"https://www.{rawrify_domain}/location?coordinates",
-    "location-full@GET": f"https://www.{rawrify_domain}/location?full"
+    "location-full@GET": f"https://www.{rawrify_domain}/location?full",
+    "encryption@POST": [f"https://www.{rawrify_domain}/encrypt", {"KEY": os.environ["ENCRYPTION_KEY"], "MESSAGE": "Test message from CICD"}]
+    "decryption@POST": [f"https://www.{rawrify_domain}/decrypt", {"KEY": os.environ["ENCRYPTION_KEY"], "MESSAGE": "gAAAAABiHZeFlMqocfYbNJoRI5bhDQN0QoDfQdDJDPRQM0XUVOdHqMr0v2PydOf8PY9KDDDHpMyNOg_5ouodHDMgek3jCS3MbLnywfXaco7C-Rwau1eE_SU="}]
 }
 
 expected_failures = {
@@ -33,7 +38,7 @@ expected_failures = {
 
 
 # Verify route succeeds using GET request
-def verify_get_success(route, url):
+def verify_success(route, url):
     resp = requests.get(url)
     resp_text = resp.text
     if "ERROR" in resp_text.upper() or resp.status_code > 399:
@@ -44,8 +49,13 @@ def verify_get_success(route, url):
 
 
 # Verify expected route failures using GET request
-def verify_get_failure(route, url):
-    resp = requests.get(url)
+def verify_failure(route, url):
+    if route.split("@")[1] == "GET":
+        resp = requests.get(url)
+    elif route.split("@")[1] == "POST":
+        resp = requests.post(url[0], url[1])
+    else:
+        raise ValueError(f"Expected either GET or POST secondary option for {route}.")
     resp_text = resp.text
     if "ERROR" not in resp_text.upper() or resp.status_code < 399:
         print(f"Expected failure {route} resulted in error message: {resp_text if 'location' not in route else '*****'}")
@@ -57,8 +67,8 @@ def verify_get_failure(route, url):
 if __name__ == "__main__":
     print("Beginning checks...")
     for route, url in routes.items():
-        verify_get_success(route, url)
+        verify_success(route, url)
     for route, url in expected_failures.items():
-        verify_get_failure(route, url)
+        verify_failure(route, url)
     print("Checks finished.")
 
